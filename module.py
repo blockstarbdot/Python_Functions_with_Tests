185f1626a25056d4a164abf2d34ea53803956f78
blockstarbdot <Brendan.hicks@stu.vertexacademies.org>
2026-03-03 19:15:50 +0000

    new code

--- module.py
+++ module.py
@@ -6,17 +6,27 @@ def two_sum(a: int, b: int) -> int:
 # remove_vowels takes a string argument and return a string with all vowels removed
 def remove_vowels(s: str) -> str:
   vowels = ["a", "e" , "i", "o", "u"]
-  string = ""
-  for char in s:
-     if char not in  vowels:
-        string = string + char
-  return string
+  output = ""
+  for char in s: # Each character in s  
+     if char.lower() not in  vowels:
+        output += char 
+  return output
 
 # factorial takes an integer argument and returns the factorial result of that integer
 # factorial must use iteration and not the built-in factorial function
 def factorial(num: int) -> int:
-    pass
+    output = 1 
+    for i in range(1, num + 1):
+        output *= i 
+         
+    return output
 
 # listify takes a string argument and returns a list of characters in the string
 def listify(s: str) -> list:
-    pass
+    output = []
+    for char in s :
+       output.append(char)
+       
+    return output
+       
+     
